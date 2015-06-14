ceff716a78bef4f90bdd1fc02203d36f85115702
Nathan Wharry <nobrakes@fullsail.edu>
2015-06-14 18:46:09 -0500

    Current Build 6-14
    Unable to get the variables to display or pass outside of the getData
    function.

--- Wunderland Weather App/Resources/app.js
+++ Wunderland Weather App/Resources/app.js
@@ -14,6 +14,9 @@ var logoView = Ti.UI.createView({
 });
 
 
+// set global currentConditions app to store elements pulled from API call
+var currentConditions = {};
+
 
 
 var getLocation = function() {
@@ -37,24 +40,29 @@ var getLocation = function() {
 		        var json = JSON.parse(this.responseText);  //convert the string to JS object notation
 		        
 		        // set variables from API response
-		       var	city = json.current_observation.display_location.city,
-		       		state = json.current_observation.display_location.state,
-		        	full = json.current_observation.display_location.full,
-		       		forecastURL = json.current_observation.forecast_url,
-		        	tempfFull = json.current_observation.temp_f,
-		        	tempfDisplay = Math.round(tempfFull),
-		        	tempcFull = json.current_observation.temp_c,
-		        	tempcDisplay = Math.round(tempcFull),
-		        	pullTime = json.current_observation.observation_time,
-		        	currentWeather = json.current_observation.weather,
-		        	iconImage = json.current_observation.icon_url,
-		        	icon = iconImage.replace("/k/","/j/");
+		       var	currentConditions = {
+		       			
+		       			city: json.current_observation.display_location.city,
+		       			state: json.current_observation.display_location.state,
+		        		full: json.current_observation.display_location.full,
+		       			forecastURL: json.current_observation.forecast_url,
+		        		tempfFull: json.current_observation.temp_f,
+		        		tempfDisplay: Math.round(json.current_observation.temp_f),
+		        		tempcFull: json.current_observation.temp_c,
+		        		tempcDisplay: Math.round(json.current_observation.temp_c),
+		        		pullTime: json.current_observation.observation_time,
+		        		currentWeather: json.current_observation.weather,
+		        		iconImage: json.current_observation.icon_url.replace("/k/","/j/"),
+		        		
+		        	};
 		        	
 		        // used to display json data
-		        // console.log(json.current_observation.display_location.city);
+		        // console.log(json.current_observation.display_location.city)
+		        
 
 		     }; //getData.onload closing
 			
+			// setup and run data pull for current location
 			getData.open("GET", locationURL);
 			getData.send();
 			
@@ -85,30 +93,14 @@ var timeView = Ti.UI.createView({
 });
 
 
-var label = Ti.UI.createLabel({
-  color: '#404040',
-  text: 'READY?',
-  height: Ti.UI.SIZE,
-  textAlign: 'center',
-  verticalAlign: Ti.UI.TEXT_VERTICAL_ALIGNMENT_CENTER,
-  font:{
-    fontSize: '55sp',
-    fontWeight: 'bold'
-  }
-});
+
 
 var logo = Ti.UI.createImageView({
 	image: 'http://icons.wxug.com/logos/PNG/wundergroundLogo_4c_horz.png',
 });
 
-topView.add(label);
-logoView.add(logo);
-
+logoView.add(logo)
 topView.add(logoView);
-
-//timeView.add(label);
-
-
-win.add(topView); // wunderground logo
+win.add(topView);
 
 win.open();
