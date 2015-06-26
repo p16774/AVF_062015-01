35d4b9a897f12ce92be51d3fe496bb00be302725
Nathan Wharry <nobrakes@fullsail.edu>
2015-06-26 14:51:11 -0500

    Recommit from Week 1 (master)
    Committing the final build from week 1 to rebuild week 2 with SQLite

--- Wunderland Weather App/Resources/app.js
+++ Wunderland Weather App/Resources/app.js
@@ -1,13 +1,19 @@
+/* Wunderland weather app so named because I misread the Weather Underground API information
+ * Developed and tested by Nathan Wharry for Advanced Visual Framework class 201506 section 01
+ * Under the direction of Professor Jennifer McCarrick
+ * Using the Titanium Framework for construction in both iOS and Android platforms.
+ */
+
+
 // create window
-
-
 var win = Ti.UI.createWindow({
-	height: Ti.UI.FULL,
+	height: Ti.UI.FILL,
 	backgroundColor: '#ffffff',
 	layout: 'vertical'
 });
 
 
+// check network and run geo functions
 if (Ti.Network.online) {
 
 	// require our geo elements	
